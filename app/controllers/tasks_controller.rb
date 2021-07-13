c9a6c250babe082e180f0a265ad49f3e1358c33e
qixi zhang <zqx0803@me.com>
2021-07-13 20:42:17 +0800

    task-manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+	before_action :find_task, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@tasks = Task.all
 	end
 
 	def show
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 	end
 
 	def new
@@ -18,24 +20,29 @@ class TasksController < ApplicationController
 	end
 
 	def edit
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 	end
 
 	def update
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 		@task.update(set_params)
 		redirect_to task_path(@task)
 	end
 
 	def destroy
-		@task = Task.find(params[:id])
+		# @task = Task.find(params[:id])
 		@task.destroy
 		redirect_to tasks_path
 	end
 
+
 	private
 
 	def set_params
-		params.require(:task).permit(:title, :details)
+		params.require(:task).permit(:title, :details, :completed)
+	end
+
+	def find_task
+		@task = Task.find(params[:id])
 	end
 end
